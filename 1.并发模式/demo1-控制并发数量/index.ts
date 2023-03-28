c714d385e9bdbd8c69d156928c2b50b9895e3441
Fate <574392788@qq.com>
2023-03-28 23:49:17 +0800

    加入注释

--- 1.并发模式/demo1-控制并发数量/index.ts
+++ 1.并发模式/demo1-控制并发数量/index.ts
@@ -2,7 +2,7 @@
 // 每个函数作为一个‘任务’，并返回一个Promise
 // 限制同时执行任务数量
 type Task<T = any> = ()=> Promise<T>;
-
+// 定义一个 Dispatch 类型，表示任务分发器函数类型
 type Dispatch = (...task:Task[]) =>void;
 // q: 请写出lodash中的防抖
 // a: https://www.lodashjs.com/docs/lodash.debounce
@@ -19,22 +19,35 @@ function debounce(func:Function,wait:number){
     }
 }
 
-function createTaskDispatch(max = 5):Dispatch{
-    const untouchedTasks:Task[] = [];
-    // 清空未处理数组
-    const drainUntouchedTasks = debounce(()=>{
-       while(max > 0 && untouchedTasks.length > 0){
-        const task = untouchedTasks.shift()!;
-        max--;
-        task().finally(()=>{
-            max++;
-            drainUntouchedTasks()
-        });
-       }
-    }
-    ,0);
-    return function dispatch(...task:Task[]):void{
-        untouchedTasks.push(...task)
+
+
+/**
+ * 创建一个任务分发器函数，用于控制同时执行的最大任务数
+ * @param max 最大任务数，默认为 5
+ * @returns 返回一个 Dispatch 函数
+ */
+function createTaskDispatch(max = 5): Dispatch {
+    const untouchedTasks: Task[] = []; // 存储未处理的任务列表
+
+    // 创建一个防抖函数，用于清空未处理的任务列表
+    const drainUntouchedTasks = debounce(() => {
+        while (max > 0 && untouchedTasks.length > 0) {
+
+            // 取出第一个任务，并将已处理的任务数加一
+            const task = untouchedTasks.shift()!;
+            max--;
+
+            // 执行任务，无论成功或失败都将已处理的任务数减一，并递归调用 clearTasks
+            task().finally(() => {
+                max++;
+                drainUntouchedTasks()
+            });
+        }
+    }, 0);
+
+    // 返回一个 Dispatch 函数，用于向任务列表中添加新的任务
+    return function dispatch(...task: Task[]): void {
+        untouchedTasks.push(...task);
         drainUntouchedTasks();
     };
 }
@@ -42,31 +55,27 @@ function createTaskDispatch(max = 5):Dispatch{
 // 测试
 const dispatch = createTaskDispatch(2);
 
-// q: 我怎么修改下面的类型报错
-
-
-const task1:Task = () => {
+// 定义三个异步任务
+const task1: Task = () => {
     return new Promise<void>((resolve) => {
         setTimeout(() => {
             console.log('task1');
             resolve();
         }, 2000);
-    }
-    );
+    });
 }
 const task2 = () => new Promise<void>((resolve) => {
     setTimeout(() => {
         console.log('task2');
         resolve();
     }, 2000);
-    }
-);
+});
 const task3 = () => new Promise<void>((resolve) => {
     setTimeout(() => {
         console.log('task3');
         resolve();
     }, 3000);
-    }
-);
+});
 
-dispatch(task1,task2,task3,task1,task1,task1);
+// 向任务分发器中添加多个异步任务
+dispatch(task1, task2, task3, task1, task1, task1);
--- 1.并发模式/demo2-控制启动和暂停/index.ts
+++ 1.并发模式/demo2-控制启动和暂停/index.ts
@@ -2,67 +2,75 @@
  * 所有任务完成后可以得到每个任务的执行结果
  * 需要两个返回方法，start用于启动任务，pause用于停止任务
  * 每个任务具有原子性，不可中断，只能在两个任务之间中断
- * @param tasks 任务列表，每个任务无参数，返回Promise  
+ * @param tasks 任务列表，每个任务无参数，返回Promise
  */
-type Task<T = any> = ()=> Promise<T>;
+//定义了一个泛型函数类型Task，该函数返回一个Promise，可以传入一个泛型T的参数
+type Task<T = any> = () => Promise<T>;
 
-function processTasks(...tasks:Task[]){
-    let isRunning = false;
-    let i = 0;
-    const result: any[] = [];
-    let prom: Promise<any> | null = null;
-    return {
-        start:  ()=>{
-           return new Promise(async(resolve,reject)=>{
-            if (prom) {
-                prom.then(resolve,reject)
-                return;
-            }
-            if (isRunning) {
-                return;
-            }
-            isRunning = true;
-            while (i < tasks.length){
-                try {
-                    console.log('ing')
-                    result.push(await tasks[i]());
-                    console.log('end')
-
-                } catch (error) {
-
-                    isRunning = false
-                    reject(error)
-                    prom = Promise.reject(error)
-                    return
-                }
-                i++;
-                if(!isRunning && i < tasks.length -1){
-                    console.log('中断')
-
-                    return;
-                }
-            }
-            isRunning = false
-            resolve(result)
-            prom = Promise.resolve(result)
-           })
-        },
-        pause:()=>{
-            isRunning = false;
+//定义processTasks函数，可以接受任意数量的Task参数，并返回一个对象，
+//start方法返回一个Promise，进行异步操作，pause方法直接修改isRunning的状态来中断任务。
+function processTasks(...tasks: Task[]) {
+  let isRunning = false; //标记任务是否正在运行
+  let i = 0; //记录已经完成的任务的数目
+  const result: any[] = []; //存储每个任务执行的结果
+  let prom: Promise<any> | null = null; //记录当前的Promise对象
+  return {
+    start: () => {
+      return new Promise(async (resolve, reject) => {
+        if (prom) {
+          // 如果前一个Promise还没有结束，则使用新的Promise等待它的结束
+          prom.then(resolve, reject);
+          return;
         }
-    }
+        if (isRunning) {
+          // 如果正在运行，则不往下执行。
+          return;
+        }
+        isRunning = true;
+        while (i < tasks.length) {
+          // 循环执行所有任务
+          try {
+            console.log("ing");
+            result.push(await tasks[i]()); // 执行并存储结果
+            console.log("end");
+          } catch (error) {
+            isRunning = false;
+            reject(error); // 任务失败，reject出错信息
+            prom = Promise.reject(error); // 记录当前Promise对象
+            return;
+          }
+          i++;
+          if (!isRunning && i < tasks.length - 1) {
+            // 如果任务被暂停，则直接返回
+            console.log("中断");
+
+            return;
+          }
+        }
+        isRunning = false;
+        resolve(result); // 所有任务执行完成，resolve结果数组
+        prom = Promise.resolve(result); // 记录当前Promise对象
+      });
+    },
+    pause: () => {
+      isRunning = false; // 修改状态，中断任务
+    },
+  };
 }
-const task:Task = () => {
-    return new Promise<void>((resolve) => {
-        setTimeout(() => {
-            console.log('task1');
-            resolve();
-        }, 2000);
-    }
-    );
-}
-const {start,pause} = processTasks(task,task,task,task,task)
-start()
+
+//定义一个Task函数task，模拟一个异步的操作，该操作将在2秒后输出一条信息，并resolve。
+const task: Task = () => {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log("task1");
+      resolve();
+    }, 2000);
+  });
+};
+
+//调用processTasks方法，并传入5个相同的Task函数，得到start和pause方法，然后调用start方法开始异步执行所有任务，之后在4秒后调用pause方法，暂停任务。
+const { start, pause } = processTasks(task, task, task, task, task);
+start();
 setTimeout(() => {
-    pause()
+  pause();
 }, 4000);
